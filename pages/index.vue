da935930080f3f02d7b45ba79d95b966eb7d33d0
AP Keerthika <keerthi2592@gmail.com>
2018-10-23 14:38:28 +0530

    Included pug sass

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,8 @@
-<template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        conferencesite
-      </h1>
-      <h2 class="subtitle">
-        My posh Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+<template lang="pug">
+.page
+  h1 Conference
 </template>
+
 
 <script>
 import Logo from '~/components/Logo.vue'
