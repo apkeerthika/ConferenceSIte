3dca5a1f31a6119273f26d34ced8137911b5f926
manojjonam <manoj.jonam.boxer@gmail.com>
2018-10-28 20:02:25 +0530

    Add speaker section

--- components/sections/Speakers.vue
+++ components/sections/Speakers.vue
@@ -2,9 +2,110 @@
 section.speakers
   .container
     h2 Featured 2018 Speakers
+    .profile-item(v-for="(s, i) in data", :key="i")
+      .featured-profile(v-for="(f, j) in s.featured_speakers", :key="j")
+        .image
+          img(:src="f.image")
+        .caption
+          p {{ f.name }}
+          p {{ f.company }}
+      .profile-sub(v-for="(s, k) in s.speakers", :key="k")
+        .image
+          img(:src="s.image")
+        .caption
+          p {{ s.name }}
+          p {{ s.company }}
 </template>
+
+
+<script>
+import speaker from './speaker'
+
+export default {
+  data () {
+    return {
+      data: speaker
+    }
+  }
+}
+</script>
 
 <style lang="sass" scoped>
 @import 'assets/styles/inbound'
 
+.profile-item
+  display: flex
+  justify-content: flex-start
+  flex-wrap: wrap
+
+.featured-profile
+  width: 31.33%
+  display: flex
+  flex-flow: column
+  position: relative
+  .caption
+    background-color: $inbound-violet
+    color: $white
+    padding: 1.6em 1.2em
+    display: inline-block
+    position: relative
+    width: 100%
+    overflow: hidden
+    flex: 1 1 auto
+    text-align: left
+    @media (max-width: 768px)
+      p
+        font-size: 2vw
+
+.featured-profile
+  margin-left: 3.00005%
+  &:first-child
+    margin-left: 0
+  &:nth-child(3n+1)
+    margin-left: 0
+  @media (max-width: 768px)
+    &:nth-child(odd)
+      margin-left: 0
+    &:nth-child(even)
+      margin-left: 3%
+
+
+.featured-profile
+  @media (max-width: 768px)
+    width: 48%
+
+.profile-sub
+  &:nth-child(4n+2)
+    margin-left: 2.8%
+  &:nth-child(4n+4)
+    margin-left: 2.8%
+  &:nth-child(4n+5)
+    margin-left: 2.8%
+.profile-sub
+  margin-top: $space*3
+  width: 22.9%
+  display: flex
+  flex-flow: column
+  position: relative
+  .image
+    img
+      width: 100%
+  .caption
+    background: $inbound-violet
+    color: $white
+    display: inline-block
+    position: relative
+    width: 100%
+    overflow: hidden
+    flex: 1 1 auto
+    box-sizing: border-box
+    padding: 1.6em
+    text-align: left
+
+
+.profile-sub
+  @media (max-width: 768px)
+    flex-flow: row
+    // .caption
+    //   display: block
 </style>
---
+++ components/sections/speaker.js
@@ -0,0 +1,94 @@
+export default [
+  {
+    class: 'featured',
+    featured_speakers: [
+      {
+        image:
+          'https://www.inbound.com/hs-fs/hubfs/Keynote%20Profile%201.png?t=1540589515044&width=338&name=Keynote%20Profile%201.png',
+        name: 'Chimamanda Ngozi Adichie',
+        company: 'Award-Winning Novelist & Writer'
+      },
+      {
+        image:
+          'https://www.inbound.com/hs-fs/hubfs/Keynote%20Profile%202-1.png?t=1540589515044&width=338&name=Keynote%20Profile%202-1.png',
+        name: 'Deepak Chopra',
+        company: 'Best-Selling Author and Founder, Chopra Foundation'
+      },
+      {
+        image:
+          'https://www.inbound.com/hs-fs/hubfs/Keynote%20Profile%205.png?t=1540589515044&width=338&name=Keynote%20Profile%205.png',
+        name: 'Beth Comstock',
+        company: 'Former Vice Chair, GE'
+      },
+      {
+        image:
+          'https://www.inbound.com/hs-fs/hubfs/Keynote%20Profile%204.png?t=1540589515044&width=338&name=Keynote%20Profile%204.png',
+        name: 'Scott Harrison',
+        company: 'Founder & CEO, charity:water'
+      },
+      {
+        image:
+          'https://www.inbound.com/hs-fs/hubfs/assets/images/speakers/Brian-Halligan-Dharmesh-Shah-INBOUND.png?t=1540589515044&width=338&name=Brian-Halligan-Dharmesh-Shah-INBOUND.png',
+        name: 'Brian Halligan & Dharmesh Shah',
+        company: 'Co-Founders, HubSpot'
+      },
+      {
+        image:
+          'https://www.inbound.com/hs-fs/hubfs/INBOUND18_ShondaRhimes_KeynoteProfile-1.png?t=1540589515044&width=338&name=INBOUND18_ShondaRhimes_KeynoteProfile-1.png',
+        name: 'Shonda Rhimes',
+        company:
+          'Award Winning Writer, Executive Producer, and Best Selling Author'
+      }
+    ],
+    speakers: [
+      {
+        image:
+          'https://www.inbound.com/hs-fs/hubfs/Spotlight%20Profile%2033-1.png?t=1540589515044&width=225&name=Spotlight%20Profile%2033-1.png',
+        name: 'Tarana Burke',
+        company: "Founder of the 'me too.' Movement"
+      },
+      {
+        image:
+          'https://www.inbound.com/hs-fs/hubfs/EmilyChang-INBOUND-color.jpg?t=1540589515044&width=225&name=EmilyChang-INBOUND-color.jpg',
+        name: 'Emily Chang',
+        company: 'Author, Brotopia & Executive Producer, Bloomberg Technology'
+      },
+      {
+        image:
+          'https://www.inbound.com/hs-fs/hubfs/TroyCarter-INBOUND-color.jpg?t=1540589515044&width=225&name=TroyCarter-INBOUND-color.jpg',
+        name: 'Troy Carter',
+        company: 'Founder & CEO, Atom Factory'
+      },
+      {
+        image:
+          'https://www.inbound.com/hs-fs/hubfs/Mo_Gawdat_Google.png?t=1540589515044&width=225&name=Mo_Gawdat_Google.png',
+        name: 'Mo Gawdat',
+        company: 'Chief Business Officer, Google X'
+      },
+      {
+        image:
+          'https://www.inbound.com/hs-fs/hubfs/INBOUND18_EstherPerel_SpotlightProfile-1.png?t=1540589515044&width=225&name=INBOUND18_EstherPerel_SpotlightProfile-1.png',
+        name: 'Esther Perel',
+        company: 'Psychotherapist and Host, Where Should We Begin?'
+      },
+      {
+        image:
+          'https://www.inbound.com/hs-fs/hubfs/Spotlight%20Profile%2023-3.png?t=1540589515044&width=225&name=Spotlight%20Profile%2023-3.png',
+        name: 'Julie Rice',
+        company: 'Co-Founder, Soulcycle & Chief Brand Officer, WeWork'
+      },
+      {
+        image:
+          'https://www.inbound.com/hs-fs/hubfs/Spotlight.png?t=1540589515044&width=225&name=Spotlight.png',
+        name: 'Alex Rodriguez',
+        company: 'Founder & Chief Executive Officer, A-Rod Corp'
+      },
+      {
+        image:
+          'https://www.inbound.com/hs-fs/hubfs/Lena%20Waithe.png?t=1540589515044&width=225&name=Lena%20Waithe.png',
+        name: 'Lena Waithe',
+        company: 'Emmy Award Winning Writer, Producer, and Actress'
+      }
+    ]
+  },
+]
