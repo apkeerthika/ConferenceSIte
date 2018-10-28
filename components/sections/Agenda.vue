662a36acc49df93cdc96134569b61b784b61e1a6
manojjonam <manoj.jonam.boxer@gmail.com>
2018-10-28 16:58:28 +0530

    Add agenda sessions

--- components/sections/Agenda.vue
+++ components/sections/Agenda.vue
@@ -2,7 +2,40 @@
 section.agenda
   .container
     h2 2018 Agenda
+    .content
+      ul
+        li
+          span(@click="select(i)", v-for="(day, i) in data", :key="i") {{ day.day }} 
+            .date {{ day.date }}
+      .sessions(v-if="selected === i", v-for="(day, i) in data", :key="i")
+        .session(v-for="(s, i) in day.sessions", :key="i")
+          p.time
+            span {{ s.start_time }} 
+            span(v-if="s.end_time") — {{ s.end_time }}
+          .marker(:class="s.class")
+            img.image(v-if="s.bg", :src="s.bg")
+          p.text {{ s.title }}
+    .cta
+      a.btn.btn-alt See The full 2018 agenda
 </template>
+
+<script>
+import session from '~/components/sections/session.js'
+export default {
+  data () {
+    return {
+      data: session,
+      selected: 0
+    }
+  },
+  methods: {
+    select (i) {
+      this.selected = i
+    }
+  }
+}
+</script>
+
 
 <style lang="sass" scoped>
 @import 'assets/styles/inbound'
@@ -10,4 +43,91 @@ section.agenda
 section.agenda
   background-color: $inbound-blue
   color: $white
+  padding: $space*5
+  .content
+    padding-top: $space*5
+
+
+.sessions
+  padding-top: $space*3
+  .session
+    @include spread
+    .time, .text
+      flex: 1
+    .time
+      text-align: right
+      padding-right: $space*3
+    .text
+      text-align: left
+      padding-left: $space*3
+      font-size: 1rem
+
+.cta
+  padding-top: $space*7
+  a
+    padding: $space*1.75 $space*5 !important
+
+.marker
+  background: transparent
+  width: 1.2rem
+  height: 1.2rem
+  border-radius: 50%
+  border: 4px solid $white
+  margin: $space*2
+  position: relative
+  &:after
+    content: ""
+    display: block
+    width: 4px
+    height: 4.5rem
+    background: $white
+    @include absolute-n
+    top: 100%
+
+.session
+  &:last-child
+    .marker
+      &:after
+        display: none
+
+.marker
+  &.image
+    background-size: contain
+    width: 3.5rem
+    height: 3.5rem
+    margin: $space !important
+
+.marker
+  &.skyblue
+    background: $inbound-lightblue
+    width: 1.2rem
+    height: 1.2rem
+    border-radius: 50%
+    border: 4px solid $white
+
+
+.marker
+  &.red
+    background: $inbound-red
+    width: 1.2rem
+    height: 1.2rem
+    border-radius: 50%
+    border: 4px solid $white
+
+//Desktop
+ul
+  display: flex
+  li
+    span
+      letter-spacing: 2px
+      font-weight: 600
+      font-size: 1.5rem
+      cursor: pointer
+    @media (min-width: 768px)
+      @include flex
+      span
+        display: inline-flex
+        justify-content: space-around
+        cursor: pointer
+        padding: $space*2
 </style>
---
+++ components/sections/session.js
@@ -0,0 +1,206 @@
+export default [
+  {
+    day: 'TUESDAY',
+    date: '9/4',
+    sessions: [
+      {
+        start_time: '5:30 PM',
+        end_time: '6:15 PM',
+        title: 'INBOUND Orientation Session',
+        class: 'skyblue'
+      },
+      {
+        start_time: '6:00 PM',
+        title: 'Club INBOUND Opens'
+      },
+      {
+        start_time: '7:00 PM',
+        end_time: '8:00 PM',
+        title: 'Deepak Chopra Keynote',
+        bg:
+          'https://www.inbound.com/hubfs/Bio%20Profile%20Copy-2.png?t=1540589515044',
+        class: 'image'
+      },
+      {
+        start_time: '8:00 PM',
+        end_time: '9:30 PM',
+        title:
+          'INBOUND Welcome Party, Brought to you by Gartner Digital Markets',
+        class: 'skyblue'
+      }
+    ]
+  },
+  {
+    day: 'WEDNESDAY',
+    date: '9/5',
+    sessions: [
+      {
+        start_time: '9: 00 AM',
+        end_time: '10: 00 AM',
+        title: 'Beth Comstock Keynote',
+        bg:
+          'https://www.inbound.com/hubfs/Bio%20Profile%20Copy%205-1.png?t=1540589515044',
+        class: 'image'
+      },
+      {
+        start_time: '10: 30 AM',
+        end_time: '11: 15 AM',
+        title: 'Anjali Sud Spotlight & Breakout Sessions',
+        class: 'red'
+      },
+      {
+        start_time: '11: 00 AM',
+        title: 'Lunch Opens'
+      },
+      {
+        start_time: '11: 45 AM',
+        end_time: '12: 30 PM',
+        title:
+          ' Mo Gawdat Spotlight, Brent Chudoba Spotlight & Breakout Sessions',
+        class: 'red'
+      },
+      {
+        start_time: '1: 00 PM',
+        end_time: '1: 45 PM',
+        title: 'Tobi Lütke Spotlight & Breakout Sessions',
+        class: 'red'
+      },
+      {
+        start_time: '2: 00 PM',
+        title: 'Lunch Closes'
+      },
+      {
+        start_time: '2: 15 PM',
+        end_time: '3: 45 PM',
+        title: 'Brian Halligan & Dharmesh Shah Keynote',
+        bg: 'https://www.inbound.com/hubfs/Bio%20Profile-2.png?t=1540589515044',
+        class: 'image'
+      },
+      {
+        start_time: '4: 15 PM',
+        end_time: '5: 00 PM',
+        title: 'HubSpot Product Announcements Spotlight & Breakout Sessions',
+        class: 'red'
+      },
+      {
+        start_time: '5: 00 PM',
+        end_time: '6: 30 PM',
+        title: 'Club INBOUND Happy Hour',
+        class: 'skyblue'
+      }
+    ]
+  },
+  {
+    day: 'THURSDAY', 
+    date: '9/6',
+    sessions: [
+      {
+        start_time: '9: 00 AM',
+        end_time: '10: 00 AM',
+        title: 'Shonda Rhimes Keynote',
+        bg:
+          'https://www.inbound.com/hubfs/INBOUND18_ShondaRhimes_BioProfile.png?t=1540589515044',
+        class: 'image'
+      },
+      {
+        start_time: '10: 30 AM',
+        end_time: '11: 15 AM',
+        title: 'Joanna Coles Spotlight & Breakout Sessions',
+        class: 'red'
+      },
+      {
+        start_time: '11: 00 AM',
+        title: 'Lunch Opens'
+      },
+      {
+        start_time: '11: 45 AM',
+        end_time: '12: 30 PM',
+        title: 'Spotlight & Breakout Sessions',
+        class: 'red'
+      },
+      {
+        start_time: '1: 00 PM',
+        end_time: '1: 45 PM',
+        title: 'Alex Rodriguez Spotlight & Breakout Sessions',
+        class: 'red'
+      },
+      {
+        start_time: '2: 00 PM',
+        title: 'Lunch Closes'
+      },
+      {
+        start_time: '2: 15 PM',
+        end_time: '3: 00 PM',
+        title: 'Lena Waithe Spotlight & Breakout Sessions',
+        class: 'red'
+      },
+      {
+        start_time: '3: 30 PM',
+        end_time: '4: 15 PM',
+        title: 'Claire Johnson Spotlight & Breakout Sessions',
+        class: 'red'
+      },
+      {
+        start_time: '4: 45 PM',
+        end_time: '5: 30 PM',
+        title: 'Julie Rice and Cleo Wade Spotlight & Breakout Sessions',
+        class: 'red'
+      },
+      {
+        start_time: '7: 30 PM',
+        end_time: '10: 00 PM',
+        title: 'INBOUND Rocks',
+        class: 'skyblue'
+      }
+    ]
+  },
+  {
+    day: 'FRIDAY', 
+    date: '9/7',
+    sessions: [
+      {
+        start_time: '9: 00 AM',
+        end_time: '10: 00 AM',
+        title: 'Scott Harrison Keynote',
+        bg:
+          'https://www.inbound.com/hubfs/Bio%20Profile%20Copy%204-1.png?t=1540589515044',
+        class: 'image'
+      },
+      {
+        start_time: '10: 30 AM',
+        end_time: '11: 15 AM',
+        title: 'Payal Kadakia Spotlight & Breakout Sessions',
+        class: 'red'
+      },
+      {
+        start_time: '11: 00 AM',
+        title: 'Lunch Opens'
+      },
+      {
+        start_time: '11: 45 AM',
+        end_time: '12: 30 PM',
+        title:
+          'Tarana Burke, Emily Chang, and Ronan Farrow Spotlight & Breakout Sessions',
+        class: 'red'
+      },
+      {
+        start_time: '1: 00 PM',
+        end_time: '1: 45 PM',
+        title: 'Troy Carter Spotlight & Breakout Sessions',
+        class: 'red'
+      },
+      {
+        start_time: '2: 00 PM',
+        title: 'Lunch Closes'
+      },
+      {
+        start_time: '2: 15 PM',
+        end_time: '3: 15 PM',
+        title: 'Chimamanda Ngozi Adichie Keynote',
+        bg:
+          'https://www.inbound.com/hubfs/Bio%20Profile%20Copy%202.png?t=1540589515044',
+        class: 'image'
+      }
+    ]
+  }
+]
